58104e023035dc8ec59fe922847984ae3ccae3c2
Mark Tomlinson <mark.tomlinson3@gmail.com>
2025-02-25 10:14:23 +1100

    Barrel file

---
+++ src/app/hooks/index.ts
@@ -0,0 +1,9 @@
+export { useAuth } from "./use-auth";
+export { useHomeFeed, useProfileFeed } from "./use-feeds";
+export { useInfiniteScroll } from "./use-infinite-scroll";
+export { useCurrentProfile, useProfile } from "./use-profile";
+export { useThread } from "./use-thread";
+export { useNotifications } from "./use-notifications";
+
+// Also export types that might be needed
+export type { NotificationType } from "./use-notifications";
