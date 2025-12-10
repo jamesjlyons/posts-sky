ca4e2a7df143b06b750c3f630ff95d2c7f5a2b93
James Lyons <hello@jameslyons.design>
2025-12-09 22:07:42 -0800

    Fix run error - Update Tailwind CSS and PostCSS dependencies, add content paths in Tailwind config, and adjust globals.css imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        hover: "rgba(0, 0, 0, 0.05)", // or any color you prefer
-        // ... other colors
+        hover: 'rgba(0, 0, 0, 0.05)',
       },
       spacing: {
-        15: "60px",
+        15: '60px',
       },
     },
   },
